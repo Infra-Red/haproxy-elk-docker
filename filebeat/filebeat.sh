8a76faa6fd7ff40ddb6f1e68d1f3129be9976f7d
Infra-Red <xaaabk@gmail.com>
2016-04-21 20:43:52 +0300

    add filebeat install

---
+++ filebeat/filebeat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ELK=
+
+wget -P /tmp/ https://download.elastic.co/beats/filebeat/filebeat_1.2.1_amd64.deb
+sudo dpkg -i /tmp/filebeat_1.2.1_amd64.deb
+curl -XPUT "http://${ELK}:9200/_template/filebeat?pretty" -d@/etc/filebeat/filebeat.template.json
+sudo mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.bak
+sudo cat > /etc/filebeat/filebeat.yml <<EOL
+filebeat:
+  prospectors:
+    -
+      paths:
+        - "/home/ubuntu/logs/*.log*"
+      document_type: json_log
+output:
+  logstash:
+    enabled: true
+    hosts:
+      - ${ELK}:5044
+    index: filebeat
+EOL
+sudo /etc/init.d/filebeat restart
